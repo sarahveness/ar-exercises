421c2ee4b6c0858f0fc044f67005ff9dc53fa5e0
Sarah Veness <sarahhveness@gmail.com>
2016-10-11 23:13:55 +0000

    added name: to work with testing in exercise 7

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,10 +3,10 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-burnaby = Store.create(annual_revenue: 3000000, mens_apparel: true, womens_apparel: true)
+burnaby = Store.create(name: "Burnaby", annual_revenue: 3000000, mens_apparel: true, womens_apparel: true)
 
-richmond = Store.create(annual_revenue: 1260000, womens_apparel: true, mens_apparel: false)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, womens_apparel: true, mens_apparel: false)
 
-gastown = Store.create(annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 puts Store.count
