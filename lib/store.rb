83f23ee414fd75a84660a93be910fe571455cb2f
Sarah Veness <sarahhveness@gmail.com>
2016-10-11 23:14:53 +0000

    updated for validation

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,8 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  # validates_associated :employees
 
+  validates :name, presence: true, length: {minimum: 3}
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
 end
+
